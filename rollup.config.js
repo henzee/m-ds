0aecb5073e99649dd4f67f206a8eacc47c8b39c2
Heikki Säily <heikki.saily@gmail.com>
2022-01-13 15:03:30 +0200

    scss support fix

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import sass from "node-sass"
+import sass from "node-sass";
+import ts from "rollup-plugin-ts";
 
 import postcss from "rollup-plugin-postcss";
+import litcss from "rollup-plugin-postcss-lit";
 
 const packageJson = require("./package.json");
 
@@ -26,15 +28,17 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ sourceMap: false, tsconfig: "./tsconfig.json" }),
+      typescript({ tsconfig: "./tsconfig.json", sourceMap: true }),
       postcss({
-        preprocessor: (content, id) => new Promise((resolve, reject) => {
-          const result = sass.renderSync({ file: id })
-          resolve({ code: result.css.toString() })
-        }),
+        preprocessor: (content, id) =>
+          new Promise((resolve, reject) => {
+            const result = sass.renderSync({ file: id });
+            resolve({ code: result.css.toString() });
+          }),
         sourceMap: false,
-        extensions: ['.scss','.css']
+        extensions: [".scss", ".css"],
       }),
+      litcss(),
     ],
   },
   {
--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,14 +1,15 @@
 import { LitElement, html, css } from "lit";
 import { customElement, query, property } from "lit/decorators.js";
-import "./button.scss";
+import style from "./button.scss";
 
 @customElement("m-btn")
 export class MButton extends LitElement {
   @property({ type: String }) label!: string;
 
+  static styles = style;
 
   render() {
     return html`
-        <button class="btn-default">${this.label}</button> `;
+        <button class="btn-default btn-secondary">${this.label}</button> `;
   }
 }
